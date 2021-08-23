34949da0df9ec35431792107241fdcb20542a160
laptopdot <uquantum@hotmail.com>
2021-08-23 23:25:28 +0800

    ortools更新

---
+++ src/main/java/com/example/plannertime/ortools/linear.java
@@ -0,0 +1,10 @@
+package com.example.plannertime.ortools;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/linear")
+public class linear {
+
+}
